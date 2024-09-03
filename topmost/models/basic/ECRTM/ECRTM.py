22b0b63c53c264fdd60cbac950ba0d79361c4272
Mạnh Đỗ <89824418+manhdo249@users.noreply.github.com>
2024-09-03 15:50:26 +0700

    Update ECRTM.py

--- topmost/models/basic/ECRTM/ECRTM.py
+++ topmost/models/basic/ECRTM/ECRTM.py
@@ -12,11 +12,28 @@ class ECRTM(nn.Module):
         Xiaobao Wu, Xinshuai Dong, Thong Thanh Nguyen, Anh Tuan Luu.
     '''
     def __init__(self, vocab_size, num_topics=50, en_units=200, dropout=0., pretrained_WE=None, embed_size=200, beta_temp=0.2, weight_loss_ECR=100.0, sinkhorn_alpha=20.0, sinkhorn_max_iter=1000):
+        """
+        Khởi tạo lớp mô hình chủ đề neural.
+    
+        Args:
+        - vocab_size (int): Kích thước từ vựng (số từ duy nhất trong tập dữ liệu).
+        - num_topics (int, optional): Số chủ đề cần tạo ra. Mặc định là 50.
+        - en_units (int, optional): Số đơn vị trong tầng mã hóa (encoder). Mặc định là 200.
+        - dropout (float, optional): Xác suất dropout để tránh overfitting. Mặc định là 0.
+        - pretrained_WE (numpy.ndarray, optional): Ma trận nhúng từ đã được huấn luyện trước (Word Embeddings). Mặc định là None.
+        - embed_size (int, optional): Kích thước vector nhúng từ. Mặc định là 200.
+        - beta_temp (float, optional): Tham số nhiệt độ cho ma trận beta. Mặc định là 0.2.
+        - weight_loss_ECR (float, optional): Trọng số cho tổn thất ECR (Embedding Clustering Regularization). Mặc định là 100.0.
+        - sinkhorn_alpha (float, optional): Tham số alpha cho thuật toán Sinkhorn. Mặc định là 20.0.
+        - sinkhorn_max_iter (int, optional): Số lần lặp tối đa cho thuật toán Sinkhorn. Mặc định là 1000.
+        """
         super().__init__()
-
+        
+        # Lưu trữ số chủ đề và tham số nhiệt độ beta
         self.num_topics = num_topics
         self.beta_temp = beta_temp
 
+        # Khởi tạo các tham số cho phân phối của chủ đề với số lượng chủ đề xác định
         self.a = 1 * np.ones((1, num_topics)).astype(np.float32)
         self.mu2 = nn.Parameter(torch.as_tensor((np.log(self.a).T - np.mean(np.log(self.a), 1)).T))
         self.var2 = nn.Parameter(torch.as_tensor((((1.0 / self.a) * (1 - (2.0 / num_topics))).T + (1.0 / (num_topics * num_topics)) * np.sum(1.0 / self.a, 1)).T))
